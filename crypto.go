0b0f6d2c93a1c30de6dd006c6bafbf0f32153ed0
caio pereira <caiomp87@gmail.com>
2022-09-25 02:03:21 -0300

    add crypto entity in a models package

--- crypto.go
+++
@@ -1,9 +0,0 @@
-package main
-
-type Crypto struct {
-	ID        uint   `json:"id"`
-	Name      string `json:"name"`
-	Network   string `json:"network"`
-	UpVotes   int    `json:"upVotes"`
-	DownVotes int    `json:"downVotes"`
-}

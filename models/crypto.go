abef9ae71339a0ff6be478dfac4594082e0f1fe6
caio pereira <caiomp87@gmail.com>
2022-09-26 20:37:36 -0300

    add createdat and updatedat fields

--- models/crypto.go
+++ models/crypto.go
@@ -1,9 +1,13 @@
 package models
 
+import "time"
+
 type Crypto struct {
-	ID        uint   `json:"id"`
-	Name      string `json:"name"`
-	Network   string `json:"network"`
-	UpVotes   int    `json:"upVotes"`
-	DownVotes int    `json:"downVotes"`
+	ID        uint      `json:"id"`
+	Name      string    `json:"name"`
+	Network   string    `json:"network"`
+	UpVotes   int       `json:"upVotes"`
+	DownVotes int       `json:"downVotes"`
+	CreatedAt time.Time `json:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt"`
 }
